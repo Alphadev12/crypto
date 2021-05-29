0af39621887a401773a1700afcba273e2a23c9ff
Alphadev12 <ravalasonn@gmail.com>
2021-05-29 16:17:22 +0300

    Gestions des services

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,13 @@ class AccueilController extends AbstractController
     /**
      * @Route("/accueil", name="accueil")
      */
-    public function index()
+    public function index(ServiceRepository $repository)
     {
+        $services = $repository->findLatest();
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'services' => $services,
         ]);
     }
 }
